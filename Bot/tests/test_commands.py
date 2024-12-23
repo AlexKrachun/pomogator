49d5d7045fdffc4c8855618acd5016f063058cad
MuratKramin <131490305+MuratKramin@users.noreply.github.com>
2024-12-23 07:40:40 +0300

    Тесты на commands,py и openai_api.py

--- Bot/tests/test_commands.py
+++ Bot/tests/test_commands.py
@@ -13,10 +13,11 @@ async def test_set_commands():
     await set_commands(mock_bot)
 
     expected_commands = [
-        BotCommand(command="/start", description="Запустить бота"),
+        BotCommand(command="/start", description="Перезапустить бота"),
         BotCommand(command="/mode", description="Выбрать модель"),
-        BotCommand(command="/delete_context", description="Удалить контекст"),
+        BotCommand(command="/new_context", description="Перейти на новый контекст"),
         BotCommand(command="/contexts", description="Выбрать контекст"),
+        BotCommand(command="/img", description="Сгенерировать изображение"),
         BotCommand(command="/profile", description="Показать профиль"),
         BotCommand(command="/pay", description="Купить подписку"),
         BotCommand(command="/help", description="Показать справку"),
